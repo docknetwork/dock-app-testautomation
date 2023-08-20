8ef1a7b8a280228f0ac6f6f96a2905e115c22588
Johnson Tinuoye <johnsontinuoye@gmail.com>
2023-08-21 00:18:18 +0100

    debug: remove 30s delay

--- src/main/java/dock/android/pageobjects/WalletHomePage.java
+++ src/main/java/dock/android/pageobjects/WalletHomePage.java
@@ -69,10 +69,6 @@ public class WalletHomePage extends BasePage {
     }
 
     public WalletHomePage createNewWallet() {
-        waitABit(30000);
-
-        String pageSource = driver.getPageSource();
-        log.info("page source: " + pageSource);
 
         click(btnCreateNewWallet).waitABit(200);
         click(Selector.contentDesc("CreateWalletBtn"));
