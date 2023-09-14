ec3ff95d834c6b5955f040a0dc7f19bf9354d58b
Johnson Tinuoye <johnsontinuoye@gmail.com>
2023-09-14 06:18:04 +0100

    chore: testing reporting

--- scripts/ParseReports.sh
+++ scripts/ParseReports.sh
@@ -3,15 +3,11 @@
 # Directory containing your XML files
 xml_directory="./build/reports/tests/test/xml/"
 
-# Initialize a variable to store the concatenated results
 all_results=""
 
-# Loop through XML files in the directory
 for xml_file in "$xml_directory"/*.xml; do
-  # Read the XML file into a variable
   xml_report=$(<"$xml_file")
 
-  # Extract information from the XML
   total_tests=$(echo "$xml_report" | grep -o 'tests="[0-9]*"' | cut -d'"' -f2 | head -n 1)
   failures=$(echo "$xml_report" | grep -o 'failures="[0-9]*"' | cut -d'"' -f2 | head -n 1)
   test_name=$(echo "$xml_report" | grep -o '<testcase name="[^"]*"' | cut -d'"' -f2 | head -n 1)
@@ -20,7 +16,6 @@ for xml_file in "$xml_directory"/*.xml; do
   # Use regex to extract numeric value from "failures"
   failures=$(echo "$failures" | grep -o '[0-9]*')
 
-  # Check if failures are greater than 1
   if [ $failures -gt 0 ]; then
     result_line="$test_name: Number of Tries - $total_tests, Failures - $failures, Duration - $duration secons"
     all_results="$all_results$result_line"$'\n'
@@ -33,5 +28,4 @@ for xml_file in "$xml_directory"/*.xml; do
   fi
 done
 
-# Set the concatenated results as an environment variable
 echo "ALL_FAILED_TESTS=$all_results" >> "$GITHUB_ENV"
