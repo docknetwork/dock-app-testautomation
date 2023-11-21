0bbc820b1d692f26fb8c90b85e35773234b8bfc0
Johnson Tinuoye <johnsontinuoye@gmail.com>
2023-11-21 09:57:10 +0100

    feat: ensure did changes when network switches

--- src/test/java/dock/android/smoketests/appResetFalse/ShareDID.java
+++ src/test/java/dock/android/smoketests/appResetFalse/ShareDID.java
@@ -1,5 +1,6 @@
 package dock.android.smoketests.appResetFalse;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -21,12 +22,23 @@ public class ShareDID extends BaseTestCaseAndroid {
         else {
             walletHomePage.enterPassCodeOneTime();
         }
+        walletHomePage.ensureMainnet();
         walletHomePage.clickDID().waitABit(2000);
         walletHomePage.clickShare();
+        walletHomePage.click(By.xpath("//*[contains(@text,'Copy DID')]"));
 
         // Verify Share DID options are displayed
         Assert.assertTrue(walletHomePage.isDisplayedByText("Copy DID"));
         Assert.assertTrue(walletHomePage.isDisplayedByText("Share DID"));
         Assert.assertTrue(walletHomePage.isDisplayedByText("did:key:"));
+
+        String mainnetDID = driver.getClipboardText();
+        walletHomePage.ensureTestnet();
+        walletHomePage.clickDID().waitABit(2000);
+        walletHomePage.clickShare().waitABit(3000);
+        walletHomePage.click(By.xpath("//*[contains(@text,'Copy DID')]"));
+        String testnetDID = driver.getClipboardText();
+
+        Assert.assertFalse(mainnetDID.equals(testnetDID));
     }
 }
